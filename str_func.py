3a2617c3b2e2eff0470af0f80dfdf28ab85411f6
Lograhan <romanchrnkv@gmail.com>
2023-04-26 20:55:38 +0300

    update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,4 +1,11 @@
 def upper_word():
+    """функция для всех букв в верхнем регистре"""
     word = input("")
     return word.upper()
+
+
+def title_word():
+    """функция для Заглавной буквы в каждом слове"""
+    word = input("")
+    return word.title()
 
